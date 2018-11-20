c6ee5667447e4c61e3e47ba556949048648d7dcc
Caresa <caresa.huddleston@gmail.com>
2018-11-20 09:09:56 -0600

    adding new background

--- config.js
+++ config.js
@@ -2,3 +2,7 @@
 exports.DATABASE_URL = process.env.DATABASE_URL || global.DATABASE_URL || 'mongodb://demo:demo1234@ds231723.mlab.com:31723/smoking-cessation';
 exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL || 'mongodb://demo:demo1234@ds231723.mlab.com:31723/smoking-cessation';
 exports.PORT = process.env.PORT || 8080;
+
+function toggle(x) {
+    x.classList.toggle("change");
+}
